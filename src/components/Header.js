46aecc8b036b08d44a971cc5a0e98c0bad7fd5b3
Patrick Jang <patrick.jang@meliusresearch.com>
2021-11-05 17:21:31 -0400

    work

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+
+  const onClick = (object) => {
+    console.log(object)
+  }
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button onClick={onClick} color='green' text='Add' />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// }
+
+export default Header
